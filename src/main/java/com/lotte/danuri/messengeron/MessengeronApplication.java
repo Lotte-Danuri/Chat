7474235c830a2b12dd75c6e7b4b6b31c03ef45c7
이충현 <a8207637@naver.com>
2022-09-07 16:50:42 +0900

    feat : Initial Commit

---
+++ src/main/java/com/lotte/danuri/messengeron/MessengeronApplication.java
@@ -0,0 +1,13 @@
+package com.lotte.danuri.messengeron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengeronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengeronApplication.class, args);
+	}
+
+}
